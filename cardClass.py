7bbbdf8b9adfe2b829523e3e62600022cbf552d4
lordemporor <matthew.bosley8@gmail.com>
2021-06-23 14:43:31 -0600

    add main, card, game placeholders

---
+++ cardClass.py
@@ -0,0 +1,5 @@
+class Card:
+    def __init__(self, countInput, fillInput, shapeInput, position="not given", cameraInfoInput="not given"):
+        self.count = countInput
+        self.fill = fillInput
+        self.shape = shapeInput
